7ac138fd8e89c301dd809da879588a8b33f9326f
Jan Everaert <jan.everaert@imec.be>
2020-03-15 11:38:42 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [error, setError] = useState(null);
+
+  return (
+    <div className="App">
+      <h1>Welkom</h1>
+      <p>We werken momenteel nog aan deze site</p>
+      <a href="mailto:jan@crashlab.be">Stuur maar een mailtje als je vragen hebt</a>
+    </div>
+  );
+}
+
+export default App;
