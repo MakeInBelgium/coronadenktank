fe4e4a28caecf208d6b0df408349ae92a42aa9bc
Jan Everaert <jan.everaert@imec.be>
2020-03-16 12:35:40 +0100

    adding gentbrugge

--- src/components/InzamelPuntenComponent.jsx
+++ src/components/InzamelPuntenComponent.jsx
@@ -5,9 +5,9 @@ const InzamelPuntenComponent = () => {
   return (
     <>
       <h2>Inzamelpunten</h2>
-      <i>Deze lijst word continue aangepast</i>
+      <i>Deze lijst word continue aangepast</i><br /><br />
       <ul>
-        <li></li>
+        <li><a href="https://www.wachtposten.be/ligging/oost-vlaanderen/35-huisartsenwachtposten-gentbrugge-kliniekstraat.html" target="_blanc">Huiswachtenpost GentBrugge</a> - open tussen 9 en 16 uur</li>
       </ul>
     </>
   )
