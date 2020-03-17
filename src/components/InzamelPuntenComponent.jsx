7e401cd19b71e660514ad8f78c356d0b58514dbd
Jan Everaert <jan.everaert@imec.be>
2020-03-17 09:05:55 +0100

    adding leuven as inzamelpunt

--- src/components/InzamelPuntenComponent.jsx
+++ src/components/InzamelPuntenComponent.jsx
@@ -9,6 +9,7 @@ const InzamelPuntenComponent = () => {
       <ul>
         <li><a href="https://www.wachtposten.be/ligging/oost-vlaanderen/35-huisartsenwachtposten-gentbrugge-kliniekstraat.html" target="_blanc">Huiswachtenpost GentBrugge</a> - open tussen 9 en 16 uur</li>
         <li><a href="https://www.facebook.com/AmbuAnt/posts/2782502748452773" target="_blanc">Ambulancecentrum Antwerpen</a> - Bellen naar 03 844 33 09 er komt iemand ze halen</li>
+        <li><a href="https://www.uzleuven.be/nl/campus-gasthuisberg" target="_blanc">campus Gasthuisberg, Herestraat 49, 3000 Leuven</a> - elke werkdag tussen 8 en 20 uur bezorgen aan het onthaal</li>
       </ul>
     </>
   )
