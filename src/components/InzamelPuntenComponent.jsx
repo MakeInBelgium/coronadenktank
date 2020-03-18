5554513f3aea03779b3143706c24228fe57cc508
Joren <joren.vs@gmail.com>
2020-03-18 16:02:01 +0100

    Add wit-gele kruis antwerpen (in mechelen)

--- src/components/InzamelPuntenComponent.jsx
+++ src/components/InzamelPuntenComponent.jsx
@@ -10,6 +10,7 @@ const InzamelPuntenComponent = () => {
         <li><a href="https://www.wachtposten.be/ligging/oost-vlaanderen/35-huisartsenwachtposten-gentbrugge-kliniekstraat.html" target="_blanc">Huiswachtenpost GentBrugge</a> - open tussen 9 en 16 uur</li>
         <li><a href="https://www.facebook.com/AmbuAnt/posts/2782502748452773" target="_blanc">Ambulancecentrum Antwerpen</a> - Bellen naar 03 844 33 09 er komt iemand ze halen</li>
         <li><a href="https://www.uzleuven.be/nl/campus-gasthuisberg" target="_blanc">campus Gasthuisberg, Herestraat 49, 3000 Leuven</a> - elke werkdag tussen 8 en 20 uur bezorgen aan het onthaal <a target="_blanc" href="https://www.uzleuven.be/nl/nieuws/oproep-voor-mondmaskers">meer info</a></li>
+        <li><a href="https://www.facebook.com/wgkantwerpen/" target="_blanc">Wit-Gele Kruis Antwerpen, Mechelsesteenweg 259 te Mechelen - Afleveren kan bij hun zetel aan de Mechelsesteenweg 259 te Mechelen. Als je er niet geraakt, kunnen ze ook opgehaald worden. <a target="_blanc" href="https://www.facebook.com/madelinedestoffenmadam/posts/2379492858749612?comment_id=2379559045409660">meer info</a></li>
       </ul>
     </>
   )
