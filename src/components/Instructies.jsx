07b92052352280230e1a2d0cb678d75dd2d2cc3f
Jan Everaert <jan.everaert@imec.be>
2020-03-18 13:12:18 +0100

    adding copy

--- src/components/Instructies.jsx
+++ src/components/Instructies.jsx
@@ -15,10 +15,9 @@ const Instructies = () => {
 
   return (
     <>
-      <h2>Zal zo snel mogelijk beschikbaar gemaakt worden</h2>
-      <p>In afwachting van de aanbevelingen van FOD Volkgezondheid raden we alle naaisters nu al aan <strong>minstens 2 laags katoen en geen elastieken lintjes te gebruiken</strong>, zodat de mondmaskers bij ontvangst door de ziekenhuizen gewassen kunnen worden op 90C met detergent.</p>
-      <p>"De professor benadrukt dat zij die zelf aan de slag willen zich best eerst melden bij een ziekenhuis of artsenpraktijk in de buurt. Daar kunnen vrijwilligers info krijgen over het juiste materiaal en de nodige informatie. De FOD Volksgezondheid heeft alvast in een tweet aangekondigd te werken aan een patroon om zelf goede maskers te maken."</p>
-      <p><a href="https://www.vrt.be/vrtnws/nl/2020/03/16/oproep-mondmaskers-ook-mensen-die-zelf-thuis-maskers-naaien-k/" target="_blanc">Bron:VRT</a></p>
+      <p>Onze zorgverleners hebben je nodig! Om de strijd tegen #COVID19BE te winnen, hebben ze dringend nood aan een aantal materialen zoals mondmaskers en schorten. Maar die zijn bijna uitgeput. Wij bezorgen hen alvast een back-up plan, met onze zelfgemaakte maskers en schorten. Ook jij kan helpen!
+      Op deze pagina delen we de goedgekeurde patronen en handleidingen.
+Haal je naaimachines boven en roep je vriend(inn)en op om mee te doen.</p>
       <div>
         <div className="value-props row">
           <div className="four columns value-prop">
