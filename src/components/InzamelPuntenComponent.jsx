1455cee589536780796f841bb0a1b4f0b3af25e6
Joren <joren.vs@gmail.com>
2020-03-18 21:35:03 +0100

    Update InzamelPuntenComponent.jsx

--- src/components/InzamelPuntenComponent.jsx
+++ src/components/InzamelPuntenComponent.jsx
@@ -11,6 +11,7 @@ const InzamelPuntenComponent = () => {
         <li><a href="https://www.facebook.com/AmbuAnt/posts/2782502748452773" target="_blanc">Ambulancecentrum Antwerpen</a> - Bellen naar 03 844 33 09 er komt iemand ze halen</li>
         <li><a href="https://www.uzleuven.be/nl/campus-gasthuisberg" target="_blanc">campus Gasthuisberg, Herestraat 49, 3000 Leuven</a> - elke werkdag tussen 8 en 20 uur bezorgen aan het onthaal <a target="_blanc" href="https://www.uzleuven.be/nl/nieuws/oproep-voor-mondmaskers">meer info</a></li>
         <li><a href="https://www.facebook.com/wgkantwerpen/" target="_blanc">Wit-Gele Kruis Antwerpen, Mechelsesteenweg 259 te Mechelen</a> - Afleveren kan bij hun zetel aan de Mechelsesteenweg 259 te Mechelen. Als je er niet geraakt, kunnen ze ook opgehaald worden. <a target="_blanc" href="https://www.facebook.com/madelinedestoffenmadam/posts/2379492858749612?comment_id=2379559045409660">meer info</a></li>
+        <li>In Gent: OCMW-gebouw in Gentbrugge (Oefenpleinstraat 6, poort 21) van 7u30 tot 16u & de lokale dienstencentra De Vlaschaard, De Knoop, Wibier, De Waterspiegel of De Mantel van 14u tot 16u. <a href="https://twitter.com/Stadgent/status/1240324009172107268" target="_blanc">meer info</a></li>
       </ul>
     </>
   )
