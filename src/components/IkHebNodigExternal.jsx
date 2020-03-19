2e5a2c3e6d6bf88b394f804def6ea368bc579200
Wouter Vandenneucker <wouter@cr3do.be>
2020-03-19 14:49:35 +0100

    update with new form links that are for more materials than only masks

--- src/components/IkHebNodigExternal.jsx
+++ src/components/IkHebNodigExternal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const IkHebNodigExternal = () => {
   return (
-    <iframe title="materiaalnodig" src="https://docs.google.com/forms/d/e/1FAIpQLScmXqJsBb67UVoqIZfXwJ5JaVdlKj0h4NbuXZ0fpqknGeK88g/viewform?embedded=true" width="640" height="2017" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>)
+    <iframe title="materiaalnodig" src="https://docs.google.com/forms/d/1Anae47KklLISFa4KPnViGQ38a9pWsEVSSlF-mteTVJM/viewform?embedded=true" width="640" height="2017" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>)
 
 }
 
